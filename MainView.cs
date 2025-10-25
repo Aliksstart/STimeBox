153a70ce232916b3c7c72adcb1f496da9d91f712
alexey.stratulat <aliksstart@mail.ru>
2025-10-25 13:17:13 +0700

    Added base project

---
+++ MainView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STimeBox
+{
+    public class MainView : Form
+    {
+        private void SetSizeWindow() {
+            Size screenSize = Support.GetPrimaryScreenSize();
+            if (!screenSize.IsEmpty) {
+                Size = new Size(Support.CalcValuePercentFormMax(screenSize.Width, 20), Support.CalcValuePercentFormMax(screenSize.Height, 75));
+            }
+        }
+        private void SetRightDownLocation() {
+            Size screenSize = Support.GetPrimaryScreenSize();
+            Location = new Point(screenSize.Width - Size.Width, screenSize.Height - Size.Height);
+        }
+        public MainView() {
+            this.Load += MainView_Load;
+            Text = "STimeBox";
+            WindowState = FormWindowState.Normal;
+            FormBorderStyle = FormBorderStyle.None;
+            SetSizeWindow();
+            BackColor = Support.ConvertHexToColor("#2fed80");
+        }
+
+        private void MainView_Load(object? sender, EventArgs e)
+        {
+            SetRightDownLocation();
+        }
+
+        public void Redner() { }
+    }
+}
---
+++ Support.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace STimeBox
+{
+    static public class Support
+    {
+        static public int CalcValuePercentFormMax(int max, double percent) { 
+            double d = Math.Floor((max*percent)  / 100);
+            return (int)d; 
+        }
+        static public Size GetPrimaryScreenSize() {
+            if (Screen.PrimaryScreen != null )
+                return new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            return Size.Empty;
+        }
+        static public Color ConvertHexToColor(string color) {
+            string a = color;
+            if (a[0] == '#') { 
+                a = a.Substring(1);
+            }
+            int r = Convert.ToInt32(a.Substring(0, 2), 16);
+            int g = Convert.ToInt32(a.Substring(2, 2), 16);
+            int b = Convert.ToInt32(a.Substring(4, 2), 16);
+
+            // Альфа-канал (непрозрачный)
+            int alp = 255;
+
+            // Создание цвета
+            return Color.FromArgb(alp, r, g, b);
+        }
+    }
+}
